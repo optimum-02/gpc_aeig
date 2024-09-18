960c1e0ffd06185df6319e3e01837a0c1b6d8c63
optimum02 <mawou.aa@e-gbavou.com>
2024-09-18 11:58:24 +0100

    backend : project scaffolding

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,30 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
-
+const express =require('express');
+const { createServer } = require('http');
+const { Server } =require('socket.io');
+const { scopePerRequest } = require('awilix-express');
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-import appConfig from './src/config/app_config.js';
+const apiRoutes = require('./routes/app_route.js');
+const container = require('./config/dependencie_injection');
+const appConfig = require('./config/app_config');
+const { connectDB } = require('./config/db');
+const { errorMiddleware } = require('./middlewares/error_middleware.js');
+
+app.use(express.json());
+app.use(scopePerRequest(container));
+app.use(apiRoutes);
+// app.use(errorMiddleware)
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
+connectDB();
+
 server.listen(appConfig.port, () => {
   console.log(`server running at http://localhost:${appConfig.port}`);
 });
 
-export default app;
+module.exports= app;
